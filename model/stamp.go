69252611610c1ff0f45b35bcc388317214d2bdc1
Ras96 <asymptote.k.k@gmail.com>
2022-06-14 20:34:49 +0900

    :sparkles: impl FindAll

--- model/stamp.go
+++ model/stamp.go
@@ -5,5 +5,6 @@ import "github.com/google/uuid"
 type Stamp struct {
 	ID       uuid.UUID `json:"id" db:"id"`
 	Name     string    `json:"name" db:"name"`
-	ImageURL string    `json:"imageUrl" db:"image_url"`
+	ImageURL string    `json:"-" db:"image_url"`
+	Image    string    `json:"image" db:"-"`
 }
--- repository/impl/stamp.go
+++ repository/impl/stamp.go
@@ -15,5 +15,22 @@ func NewStampRepository(db *sqlx.DB) repository.StampRepository {
 }
 
 func (r *stampRepository) FindAll() ([]model.Stamp, error) {
-	return []model.Stamp{}, nil
+	var stamps []model.Stamp
+
+	err := r.db.Select(&stamps, "SELECT * FROM stamps")
+	if err != nil {
+		return nil, err
+	}
+
+	for i, s := range stamps {
+		image := getImage(s.ImageURL) // TODO: 可能ならまとめて取得する
+		stamps[i].Image = image
+	}
+
+	return stamps, nil
+}
+
+// TODO: ストレージから取得する
+func getImage(url string) string {
+	return "aG9nZWhvZ2Vob2dlaG9nZQ=="
 }
