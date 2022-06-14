583d665d3c8f0bd02ab1db24bc049c47f1a853f6
Ras96 <asymptote.k.k@gmail.com>
2022-06-14 20:28:45 +0900

    :sparkles: [wip] Add stamp model

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hackathon-22-spring-14/hackathon-22-spring-14-backend
 go 1.18
 
 require (
+	github.com/google/uuid v1.3.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/labstack/echo/v4 v4.7.2
 )
--- model/stamp.go
+++ model/stamp.go
@@ -1,3 +1,9 @@
 package model
 
-type Stamp struct {}
+import "github.com/google/uuid"
+
+type Stamp struct {
+	ID       uuid.UUID `json:"id" db:"id"`
+	Name     string    `json:"name" db:"name"`
+	ImageURL string    `json:"imageUrl" db:"image_url"`
+}
