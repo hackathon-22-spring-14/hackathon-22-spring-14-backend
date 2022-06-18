5be1b36e7093a750039e73623c353257261c6e24
kyosu-1 <sho013039@gmail.com>
2022-06-18 16:38:14 +0900

    update S3

--- repository/impl/s3.go
+++ repository/impl/s3.go
@@ -1,71 +1,77 @@
 package impl
 
 import (
-    "context"
-    "io"
-    "log"
-    "os"
-    "encording/base64"
+	"bytes"
+	"context"
+	"encoding/base64"
+	"errors"
+	"log"
+	"os"
 
-    "github.com/aws/aws-sdk-go-v2/aws"
-    "github.com/aws/aws-sdk-go-v2/feature/s3/manager"
-    "github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-const bucket = os.GETenv("BUCKET_NAME")
+var bucket = os.Getenv("BUCKET_NAME")
 
 type stampStrage struct {
-    downloader *manager.Downloader
-    uploader   *manager.Uploader
-    client     *s3.Client
+	downloader *manager.Downloader
+	uploader   *manager.Uploader
+	client     *s3.Client
 }
 
 func NewStampStrage(cfg aws.Config) *stampStrage {
+	client := s3.NewFromConfig(cfg)
+	downloader := manager.NewDownloader(client)
+	uploader := manager.NewUploader(client)
 
-    client := s3.NewFromConfig(cfg)
-    downloader := manager.NewDownloader(client)
-    uploader := manager.NewUploader(client)
-
-    return &stampStrage{
-        downloader: downloader,
-        uploader:   uploader,
-        client:     client,
-    }
+	return &stampStrage{
+		downloader: downloader,
+		uploader:   uploader,
+		client:     client,
+	}
 }
 
 // オブジェクトをS3にアップロードする
-func (c *stampStrage) UploadSingleObject(path string, image io.Readeret) error {
-    _, err := c.uploader.Upload(context.Background(), &s3.PutObjectInput{
-        Bucket: aws.String(bucket),
-        Key:    aws.String(key),
-        Body:   image,
-    })
+func (c *stampStrage) UploadSingleObject(path string, image string) error {
+	imageByte, err := base64.StdEncoding.DecodeString(image)
+	if err != nil {
+		return nil
+	}
 
-    if err != nil {
-        log.Fatal(err)
-        return err
-    }
+	imageReadeer := bytes.NewReader(imageByte)
 
-    return nil
+	_, err = c.uploader.Upload(context.Background(), &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(path),
+		Body:   imageReadeer,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	return nil
 }
 
 // オブジェクトをS3からダウンロードする
-func (c *stampStrage) DownloadSingleObject(path string) error {
+func (c *stampStrage) DownloadSingleObject(path string) (string, error) {
 
-    buffer := manager.NewWriteAtBuffer([]byte{})
+	buffer := manager.NewWriteAtBuffer([]byte{})
+	numBytes, err := c.downloader.Download(context.TODO(), buffer, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(path),
+	})
 
-    numBytes, err := c.downloader.Download(context.TODO(), buffer, &s3.GetObjectInput{
-        Bucket: aws.String(bucket),
-        Key:    aws.String(path),
-    })
+	if err != nil {
+		return "", err
+	}
 
-    if err != nil {
-        return nil, err
-    }
+	if numBytes < 1 {
+		return "", errors.New("zero bytes written to memory")
+	}
 
-    if numBytes < 1 {
-        return nil, errors.New("zero bytes written to memory")
-    }
-
-    return base64.StdEncoding.EncodeToString(buffer.Bytes(),) nil
+	return base64.StdEncoding.EncodeToString(buffer.Bytes()), nil
 }
